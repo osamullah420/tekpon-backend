f5ca65de28f39e17912a423fc19d69c271fa9a1f
Muhammad Saqib <developer.msaqib@gmail.com>
2024-12-16 00:56:28 +0500

    Get Sub Cat by it route added

--- Routes/subCategoryRoutes.js
+++ Routes/subCategoryRoutes.js
@@ -1,27 +1,29 @@
 import express from "express";
 import {
-	getSubCategoriesByCategory,
-	getAllSubCategories,
-	addSubCategory,
-	getTopSubCategories,
-	deleteSubCategory,
-	updateSubCategory,
-	getAllSubCategoriesWithDescription,
+  getSubCategoriesByCategory,
+  getAllSubCategories,
+  getSubCategoryById,
+  addSubCategory,
+  getTopSubCategories,
+  deleteSubCategory,
+  updateSubCategory,
+  getAllSubCategoriesWithDescription,
 } from "../Controllers/subCategoryController.js";
 
 const subCategoryRouter = express.Router();
 
 // Route to get subcategories by category
 subCategoryRouter.get(
-	"/:categoryId/get-sub-categories",
-	getSubCategoriesByCategory
+  "/:categoryId/get-sub-categories",
+  getSubCategoriesByCategory
 );
+subCategoryRouter.get("/get-sub-category-by-id/:id", getSubCategoryById);
 
 //get subcategories with pagination (24 per page)
 subCategoryRouter.get("/get-all-subcategories", getAllSubCategories);
 subCategoryRouter.get(
-	"/get-all-subcategories-with-description",
-	getAllSubCategoriesWithDescription
+  "/get-all-subcategories-with-description",
+  getAllSubCategoriesWithDescription
 );
 
 subCategoryRouter.post("/add-subcategory", addSubCategory);
@@ -29,8 +31,8 @@ subCategoryRouter.post("/add-subcategory", addSubCategory);
 subCategoryRouter.get("/get-popular-subcategories", getTopSubCategories);
 
 subCategoryRouter.delete(
-	"/delete-subcategory/:subCategoryId",
-	deleteSubCategory
+  "/delete-subcategory/:subCategoryId",
+  deleteSubCategory
 );
 subCategoryRouter.put("/update-subcategory/:subCategoryId", updateSubCategory);
 
