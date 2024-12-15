cb3c2626a296e8ade4302453d80000159d979b88
Osam-ullah <osamullah@rtechnologiespk.com>
2024-12-15 15:30:51 +0500

    admin

--- Routes/adminRoutes.js
+++ Routes/adminRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { login, register } from "../Controllers/adminController.js";
 
-const router = express.Router();
+const adminRouter = express.Router();
 
 // Login and Register routes
-router.post("/login", login);
-router.post("/register", register);
+adminRouter.post("/login", login);
+adminRouter.post("/register", register);
 
-export default router;
+export default adminRouter;
